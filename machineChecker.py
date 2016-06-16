fb67efda5e30214df5d967f17c87a50721a74655
root <root@directory.(none)>
2016-06-16 14:17:57 +0200

    Threshold and other setting passable via command line now.

--- machineChecker.py
+++ machineChecker.py
@@ -3,19 +3,22 @@
 from pyEdimax.libedimax import EdimaxDevice
 from mcutils.movingaverage import MovingAverage
 
+import argparse
+
 import time
 
 class MachineChecker:
 	mPowerAverageThresholdOn = 0.0
 	mPowerAverageThresholdOff = 0.0
 	
-	mMa = MovingAverage(8)
+	mMa = None
 	mEdimaxDevice = None
 
-	def __init__( self, thresholdOn, thresholdOff , machineHost ):
+	def __init__( self, thresholdOn, thresholdOff , numAvgValues ,machineHost ):
 		self.mEdimaxDevice = EdimaxDevice(machineHost)
 		self.mPowerAverageThresholdOn = thresholdOn
 		self.mPowerAverageThresholdOff = thresholdOff
+		self.mMa = MovingAverage(numAvgValues)
 	
 	def mainloop(self):
 		while True:
@@ -26,7 +29,14 @@ class MachineChecker:
 			time.sleep(2)		
 
 def main():
-	mc = MachineChecker(4,8,'192.168.176.34')
+	parser = argparse.ArgumentParser();
+	parser.add_argument('--thresh-on',help='The switch on threshold',required=True)
+	parser.add_argument('--thresh-off',help='The switch off threshold',required=True)
+	parser.add_argument('--average-numbers',help='The number of values to calculate the average on. Default is 8.',default='8')
+	parser.add_argument('host',help='hostname')
+	args= parser.parse_args()
+	#print args.thresh_on
+	mc = MachineChecker(float(args.thresh_on),float(args.thresh_off),int(args.average_numbers),args.host)
 	mc.mainloop()
 
 if __name__ == "__main__":
